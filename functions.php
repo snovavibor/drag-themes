0824186f1ae929f830d404b97866cc62f5f1ed4b
snovavibor <bzos@protonmail.com>
2019-11-20 18:26:39 +0200

    js del

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 function drag_scripts() {
 	wp_enqueue_style( 'main-style', get_stylesheet_uri(),['bootstrap-style'] );
 	wp_enqueue_style( 'bootstrap-style', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
-	wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+	
 }
 
 
