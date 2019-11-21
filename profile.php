f4ae5d0b9d59727e08e4790518d0507abe08b503
snovavibor <bzos@protonmail.com>
2019-11-21 02:12:50 +0200

    dd

--- profile.php
+++ profile.php
@@ -10,7 +10,7 @@ Template Name: Profile Template
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<!-- Цикл WordPress -->
 	<div class="container">
-	<a href="/" class="btn btn-success"><< BACK</a>
+	<a href="<?php bloginfo('url'); ?>" class="btn btn-success"><< BACK</a>
     <h1>Profile USER</h1>
 	
     
